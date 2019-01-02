4f0bca292f70f9e8ce36fba61d858f1ed3fe2dab
TRHX <tanrenhou@126.com>
2019-01-02 23:20:22 +0800

    Site updated: 2019-01-02 23:20:09

--- js/pc.js
+++ js/pc.js
@@ -1,9 +1,6 @@
 define([], function(){
-
     var Tips = (function(){
-
         var $tipBox = $(".tips-box");
-
         return {
             show: function(){
                 $tipBox.removeClass("hide");
@@ -12,11 +9,9 @@ define([], function(){
                 $tipBox.addClass("hide");
             },
             init: function(){
-                
             }
         }
     })();
-
     var resetTags = function(){
         var tags = $(".tagcloud a");
         tags.css({"font-size": "12px"});
@@ -26,7 +21,6 @@ define([], function(){
             tags.eq(i).addClass("color"+num);
         }
     }
-
     var slide = function(idx){
         // 修复IE10+切换无效的bug
         var $wrap = $(".switch-wrap"),
@@ -44,7 +38,6 @@ define([], function(){
         $(".icon-wrap").addClass("hide");
         $(".icon-wrap").eq(idx).removeClass("hide");
     }
-
     var bind = function(){
         var switchBtn = $("#myonoffswitch");
         var tagcloud = $(".second-part");
@@ -61,11 +54,9 @@ define([], function(){
                 resetTags();
             }
         });
-
         var timeout;
         var isEnterBtn = false;
         var isEnterTips = false;
-
         $(".icon").bind("mouseenter", function(){
             isEnterBtn = true;
             Tips.show();
@@ -77,7 +68,6 @@ define([], function(){
                 }
             }, 100);
         });
-
         $(".tips-box").bind("mouseenter", function(){
             isEnterTips = true;
             Tips.show();
@@ -89,16 +79,12 @@ define([], function(){
                 }
             }, 100);
         });
-
         $(".tips-inner li").bind("click", function(){
             var idx = $(this).index();
             slide(idx);
             Tips.hide();
         });
     }
-
-    
-
     return {
         init: function(){
             resetTags();
