896b058e5b3209d819e2c98096912252050f93a8
TRHX <tanrenhou@126.com>
2019-12-29 14:23:26 +0800

    Site updated: 2019-12-29 14:22:53

--- js/FunnyTitle.js
+++ js/FunnyTitle.js
@@ -3,12 +3,12 @@ var OriginTitle = document.title;
 var titleTime;
 document.addEventListener('visibilitychange', function () {
     if (document.hidden) {
-        $('[rel="icon"]').attr('href', "https://cdn.jsdelivr.net/gh/TRHX/CDN-for-itrhx.com@3.0.2/images/failure.ico");
+        $('[rel="icon"]').attr('href', "https://cdn.jsdelivr.net/gh/TRHX/CDN-for-itrhx.com@3.0.3/images/failure.ico");
         document.title = '╭(°A°`)╮ 页面崩溃啦 ~';
         clearTimeout(titleTime);
     }
     else {
-        $('[rel="icon"]').attr('href', "https://cdn.jsdelivr.net/gh/TRHX/CDN-for-itrhx.com@3.0.2/images/failure.ico");
+        $('[rel="icon"]').attr('href', "https://cdn.jsdelivr.net/gh/TRHX/CDN-for-itrhx.com@3.0.3/images/favicon.ico");
         document.title = '(ฅ>ω<*ฅ) 噫又好了 ~' + OriginTitle;
         titleTime = setTimeout(function () {
             document.title = OriginTitle;
